b61cbccc068c193775309a8f86c325cb241ff3fa
LashaGOG <gogritchianilasha@gmail.com>
2022-03-08 20:18:42 +0100

    Partie 1.

--- primalite.c
+++ primalite.c
@@ -15,57 +15,146 @@ int is_prime_naive(long p)
 
 long mod_pow_naive(long a, long m, long n)
 {
-	/* Prend en entrée trois entiers a, m et n, et qui retourne la valeur a^b mod n par la métchode naïve */
-	long mod = 1;
+	/* Prend en entrée trois entiers a, m et n, et qui retourne la valeur a^m mod n par la métchode naïve */
+	long val_courant = 1;
 	for (int i = 0; i < m; i++)
 	{
-		mod *= a;
-		mod = mod % n;
+		val_courant *= a;
+		val_courant = val_courant % n;
 	}
-	return mod;
+	return val_courant;
 }
 
 int mod_pow(long a, long m, long n)
 {
 	/* Realise meme operation que la fonction mod_pow_naive, sauf pour obtenir la complexité logarithmique élévation est fait au carré */
-	if (m == 1)
-	{
-		return a % n;
-	}
+	// Les cas de base
+	if (a == 0)
+		return 0;
+	if (m == 0)
+		return 1;
+
+	// Si m est paire
+	long b;
 	if (m % 2 == 0)
 	{
-		int mod = mod_pow(a, (long)(m / 2), n);
-		return (mod * mod) % n;
+		b = mod_pow(a, m / 2, n);
+		b = (b * b) % n;
 	}
+
+	// Si m est impaire
 	else
 	{
-		int mod = mod_pow(a, (long)((m - 1) / 2), n);
-		return (mod * mod * (a % n)) % n;
+		b = a % n;
+		b = (b * mod_pow(a, m - 1, n) % n) % n;
 	}
+	return (int)((b + n) % n);
 }
-int main()
+int witness(long a, long b, long d, long p)
 {
-	clock_t temps_initial;
-	clock_t temps_final;
-	double temps1, temps2;
-	long a = 5821352136521;
-	long m;
-	long n = 6525251414;
-	char nom[10] = "temps.txt";
-	FILE *fichier = fopen(nom, "w");
-	for (m = 10000000; m <= 300000000; m += 10000000)
+	/* teste si a est un témoin de Miller pour p, pour un entier a donné */
+	long x = mod_pow(a, d, p);
+	if (x == 1)
 	{
-		temps_initial = clock();
-		mod_pow_naive(a, m, n);
-		temps_final = clock();
-		temps1 = (double)(temps_final - temps_initial) / CLOCKS_PER_SEC;
-
-		temps_initial = clock();
-		mod_pow(a, m, n);
-		temps_final = clock();
-		temps2 = (double)(temps_final - temps_initial) / CLOCKS_PER_SEC;
-
-		fprintf(fichier, "%ld %lf %lf\n", m, temps1, temps2);
+		return 0;
 	}
-	fclose(fichier);
+	for (long i = 0; i < b; i++)
+	{
+		if (x == p - 1)
+		{
+			return 0;
+		}
+		x = mod_pow(x, 2, p);
+	}
+	return 1;
 }
+
+long rand_long(long low, long up)
+{
+	return rand() % (up - low + 1) + low;
+}
+
+int is_prime_miller(long p, int k)
+{
+	if (p == 2)
+	{
+		return 1;
+	}
+	if (!(p & 1) || p <= 1) // on verifie que p est impair et different de 1
+	{
+		return 0;
+	}
+	// on determine b et d :
+	long b = 0;
+	long d = p - 1;
+	while (!(d & 1)) // tant que d n’est pas impair
+	{ 
+		d = d/2;
+		b=b+1;
+	}
+	// On genere k valeurs pour a, et on teste si c’est un temoin :
+	long a;
+	int i;
+	for(i = 0;i<k;i++) 
+	{
+		a= rand_long(2, p-1);
+		if(witness(a,b,d,p))
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+long random_prime_number(int low_size, int up_size, int k)
+{
+	/* retourne un nombre premier de taille comprise entre low_size et up_size */
+	long val_low = 1; 
+	int t; 
+	// on obtient la valeure minimal
+	for (t = 0; t <low_size; t++) 
+	{
+		val_low *= 2; 
+	}
+	// on obtient la valeure maximale
+	long val_up = val_low;
+	while (t < up_size)
+	{
+		val_up *= 2; 
+		t ++;
+	}
+	val_up-- ;
+
+	// on cherche le nb premier entre les valeurs obtenus
+	long p = rand_long (val_low,val_up); 
+	while (is_prime_miller(p,k) != 1) 
+	{
+		p = rand_long(val_low,val_up); 
+	}
+	return p; 
+}
+// int main()
+// {
+// 	clock_t temps_initial;
+// 	clock_t temps_final;
+// 	double temps1, temps2;
+// 	long a = 5821352136521;
+// 	long m;
+// 	long n = 6525251414;
+// 	char nom[10] = "temps.txt";
+// 	FILE *fichier = fopen(nom, "w");
+// 	for (m = 10000000; m <= 300000000; m += 10000000)
+// 	{
+// 		temps_initial = clock();
+// 		mod_pow_naive(a, m, n);
+// 		temps_final = clock();
+// 		temps1 = (double)(temps_final - temps_initial) / CLOCKS_PER_SEC;
+
+// 		temps_initial = clock();
+// 		mod_pow(a, m, n);
+// 		temps_final = clock();
+// 		temps2 = (double)(temps_final - temps_initial) / CLOCKS_PER_SEC;
+
+// 		fprintf(fichier, "%ld %lf %lf\n", m, temps1, temps2);
+// 	}
+// 	fclose(fichier);
+//  }
---
+++ protocole_rsa.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "primalite.c"
+long extended_gcd (long s, long t, long *u, long *v) 
+{
+    if (s == 0) 
+    {
+        *u = 0; 
+        *v = 1; 
+        return t; 
+    }
+    long uPrim, vPrim ; // Pour stocker les valeurs obtenus avec les appels recursifs
+    long gcd = extended_gcd (t%s, s, &uPrim, &vPrim); 
+    
+    // mise à jour des valeurs de u et v en utilisant les resultats des appels recursifs
+    *u = vPrim -(t/s)*uPrim; 
+    *v = uPrim; 
+
+    return gcd ; 
+}
+
+void generate_keys_values (long p, long q, long* n, long* s, long* u) 
+{
+    /* generer la cle publique pKey = (s,n) et la cle secrete sKey = (u,n) a partir des nombres premiers p et q en suivant le protocole RSA */ 
+
+    *n = p * q; 
+    long t = (p-1) * (q-1);
+    
+    long v; 
+    // generer l'entier s inferieur à t jusqu'à en trouver un tel que PGCD(s, t) = 1
+    
+    srand(time(NULL)); 
+    *s = rand() % t; 
+    while (extended_gcd(*s, t, u, &v) != 1) 
+    {
+        *s = rand() % t;
+    }
+}
+
+long *encrypt (char *chaine, long s, long n) 
+{
+    /* chiffre la chaine de caractère chaine avec la clé publique (s,n) */
+    int taille = strlen (chaine); 
+    long *msg_chiffre = (long*) malloc (taille * sizeof(long)); 
+    int i = 0, j = 0 ; 
+    while (chaine[i] != '\0') 
+    {
+        msg_chiffre[j] = mod_pow(chaine[i],s,n);
+        j++; 
+        i++;
+
+    }
+    return msg_chiffre; 
+}
+char *decrypt (long* crypted, int size, long u, long n) 
+{
+    /* dechiffre le msg chiffré avec la clé privée */
+    char *msg = (char*)malloc ((size+1)*sizeof(char)); 
+    for (int i = 0; i < size; i++) {
+        printf("%d\n",mod_pow(crypted[i],u,n));
+        msg[i] = (char)(mod_pow(crypted[i],u,n)) ;
+    }
+    msg[size] = '\0'; 
+    return msg ; 
+}
+void print_long_vector (long *result, int size) 
+{
+    printf ("Vecteur: [");
+    for (int i = 0; i < size; i++) 
+    {
+        printf ("%lx\t",result[i]);
+    }
+    printf("]\n");
+}
+int main () {
+    srand(time(NULL));
+    //Generation de cle :
+    long p = random_prime_number(3,7, 5000);
+    long q = random_prime_number(3,7, 5000);
+    while(p==q) 
+    {
+        q = random_prime_number(3,7, 5000);
+    }
+    long n, s, u;
+    generate_keys_values(p,q,&n,&s,&u);
+    //Pour avoir des cles positives :
+    if (u<0)
+    {
+        long t = (p-1)*(q-1);
+        u = u+t; //on aura toujours s*u mod t = 1
+    }
+    //Afichage des cles en hexadecimal
+    printf("cle publique = (%lx , %lx ) \n",s,n);
+    printf("cle privee = (%lx , %lx) \n",u,n);
+
+    //Chiffrement:
+    char mess[10] = "Hello";
+    int len = strlen(mess);
+    long* crypted = encrypt(mess, s, n); 
+
+    printf ("Initial message : %s \n",mess);
+    printf("Encoded representation : \n");
+    print_long_vector(crypted, len);
+
+    //Dechiffrement:
+
+    char* decoded = decrypt(crypted,len, u, n);
+    printf("Decoded : %s\n", decoded); 
+
+    return 0;
+}
